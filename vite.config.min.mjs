d7a5b2180433a78792318921eb8bb82cba347d41
Martin Spier <spiermar@gmail.com>
2026-03-03 23:58:45 -0600

    build: add minified version of scripts

---
+++ vite.config.min.mjs
@@ -0,0 +1,40 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import path from "path";
+import { fileURLToPath } from "url";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+    root: "src",
+    publicDir: "../public",
+    build: {
+        outDir: "../dist",
+        emptyOutDir: false,
+        copyPublicDir: false,
+        cssCodeSplit: true,
+        lib: {
+            entry: resolve(__dirname, "lib/flamegraph.js"),
+            name: "flamegraph",
+            formats: ["umd", "es"],
+            fileName: (format) => `d3-flamegraph.${format}.min.js`,
+        },
+        rollupOptions: {
+            external: ["d3"],
+            output: {
+                format: "umd",
+                exports: "named",
+                globals: { d3: "d3" },
+            },
+        },
+        minify: true,
+    },
+    plugins: [libInjectCss()],
+    server: {
+        port: 3000,
+    },
+    optimizeDeps: {
+        include: ["d3"],
+    },
+});
